e20d6124331f61ab80c571e63c6675e1a3380314
chiano34 <ravkagavka1@gmail.com>
2024-09-23 11:17:48 +0300

    Лаб 1.4 добавлен вторичный конструктор

--- src/Student.kt
+++ src/Student.kt
@@ -1,10 +1,11 @@
 class Student{
-    var id: String=""
+    var id: Int=0
         get(){
             return field
         }
         set(value){
-            id=value
+            if(value>0)
+                field=value
         }
     var lastName: String=""
         get() {
@@ -59,6 +60,17 @@ class Student{
         lastName=_lastName
         firstName=_firstName
         surname=_surname
+    }
+    constructor(_id:Int=0,_lastName:String,_firstName:String,_surname:String,
+                _phone:String?=null,_telegram:String?=null,_email:String?=null,_git:String?=null){
+        id = _id
+        lastName=_lastName
+        firstName=_firstName
+        surname=_surname
+        phone=_phone
+        telegram=_telegram
+        email=_email
+        git=_git
     }
     fun printObj()
     {
