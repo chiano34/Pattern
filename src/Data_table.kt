fb881af3676bb74aadf93414e4ba1cb67f4f0774
chiano34 <ravkagavka1@gmail.com>
2024-11-24 18:47:08 +0300

    Лаб 2.2.2 добавлен методы для получения данных для Data_table

--- src/Data_table.kt
+++ src/Data_table.kt
@@ -1,2 +1,11 @@
 class Data_table(private val data: Array<Array<Any>>) {
+    fun getElem(row: Int, col: Int): Any? {
+        return data.getOrNull(row)?.getOrNull(col)
+    }
+    fun getRows():Int {
+        return data.size
+    }
+    fun getCols():Int {
+        return data[0].size
+    }
 }
