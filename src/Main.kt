6d92267932ad3e47f3b9ce36b9158cbe17f7c596
chiano34 <ravkagavka1@gmail.com>
2024-12-02 13:54:57 +0300

    Лаб 3.1 добавлен метод сортировки и получения элементов

--- src/Main.kt
+++ src/Main.kt
@@ -2,13 +2,15 @@ import Student
 import Student_short
 import Data_list_student_short
 import java.text.NumberFormat.Style
+import Student_list_txt
 
 fun main() {
     val ba=Student("Smith Bob Sun phone=+79891214092 telegram=@sdasd email=smth@get.ru git=chino34/git")
     val bob=Student("Smith","Bob","John", _phone = "+79891214092" )
     val anna=Student("Smith","Anna","Helen",_phone = "89891214092", _git = "chiano34/pattern")
     val tom=Student("Johnson","Tom","William",_phone = "9891214092")
-//    val list: MutableList<Student> = mutableListOf(bob,anna,tom,ba)
+    val tom2=Student("Johnson","Tomm","William",_phone = "9891214092")
+    val list: MutableList<Student> = mutableListOf(ba,bob,anna,tom,tom2)
 //    var list2= mutableListOf<Student>()
 //    Student.write_to_txt("src/","out.txt",list)
 //    list2=Student.read_from_txt("src/out.txt")
@@ -16,21 +18,31 @@ fun main() {
 //    for(student in list2){
 //        student.printObj()
 //    }
-    val newbob=Student_short(bob)
-    val newanna=Student_short(anna)
-    val newtom=Student_short(tom)
-    val newba=Student_short(ba)
-    var short_list=Data_list_student_short(mutableListOf(
-        newbob,newanna,newba,newtom
-    ))
-    var short_table=short_list.get_table()
-    for (i in 0..short_table.getRows()-1)
-    {
-        for(j in 0..short_table.getCols()-1)
-        {
-            print(short_table.getElem(i,j))
-            print(" ")
-        }
-        println()
+//    val newbob=Student_short(bob)
+//    val newanna=Student_short(anna)
+//    val newtom=Student_short(tom)
+//    val newba=Student_short(ba)
+//    var short_list=Data_list_student_short(mutableListOf(
+//        newbob,newanna,newba,newtom
+//    ))
+//    var short_table=short_list.get_table()
+//    for (i in 0..short_table.getRows()-1)
+//    {
+//        for(j in 0..short_table.getCols()-1)
+//        {
+//            print(short_table.getElem(i,j))
+//            print(" ")
+//        }
+//        println()
+//    }
+    var n=3
+    var k=2
+    var list_student=list.drop((k-1)*n).take(n)
+    for ( student in list_student){
+        student.printObj()
     }
+//    var a=Student_list_txt()
+//    a.read_from_txt("src/out.txt")
+//    var list2=a.get_k_n_student_short_list(2,2)
+//    list2.get_data()
 }
--- src/Student_list_txt.kt
+++ src/Student_list_txt.kt
@@ -3,10 +3,10 @@ import java.io.FileNotFoundException
 import java.io.IOException
 
 class Student_list_txt {
+    var list_data:MutableList<Student> = mutableListOf<Student>()
     fun read_from_txt(address:String):MutableList<Student>{
         val file= File(address)
         var list= mutableListOf<Student>()
-        print(file.absolutePath)
         try{
             var lines=file.readLines()
             for (line in lines){
@@ -19,6 +19,10 @@ class Student_list_txt {
         catch(e: IOException){
             println("could not read file")
         }
+        list_data=list
+        for (student in list_data){
+            student.printObj()
+        }
         return list
     }
     fun write_to_txt(adress:String,name:String, students:MutableList<Student>){
@@ -27,10 +31,9 @@ class Student_list_txt {
             file.writeText(student.toString())
         }
     }
-    fun get_by_id(id:Int, path: String):Student?
+    fun get_by_id(id:Int):Student?
     {
-        val list = read_from_txt(path)
-        for(student in list)
+        for(student in list_data)
         {
             if(student.id==id)
             {
@@ -38,5 +41,17 @@ class Student_list_txt {
             }
         }
         return null
+    }
+    fun get_k_n_student_short_list(k:Int,n:Int):Data_list<Student_short>{
+        require(n*k-1 in list_data.indices)
+        var list_student=list_data.drop((k-1)*n).take(k)
+        for(student in list_student){
+            student.printObj()
+        }
+        var short_list=list_student.map{Student_short(it)}
+        return Data_list(short_list)
+    }
+    fun sort_by_name(){
+        list_data.sortBy { it.getFIO() }
     }
 }
